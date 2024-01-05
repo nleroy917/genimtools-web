ae4e89b77bdd4e810cc1c518c676dc64626c0107
Nathan LeRoy <NLeRoy917@gmail.com>
2024-01-04 21:54:25 -0500

    things are failing but I am tired

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,41 +3,52 @@ import { useState } from 'react';
 
 export default function Home() {
   const [inputBed, setInputBed] = useState<string>('');
-  const [universe, setUniverse] = useState<string>('tiles1000.hg38.bed');
-  const runCount = () => {
-    const totalRegions = count_regions(inputBed);
-    alert('Total regions: ' + totalRegions);
-  };
+  const [universeFile, setUniverseFile] = useState<string>('');
+
   return (
     <main className="flex min-h-screen flex-col p-24 gap-2">
       <div className="flex flex-col">
         <label className="font-bold">1. Select universe</label>
-        <select
-          className="border-2 border-black rounded-md p-2 bg-white"
-          value={universe}
-          onChange={(e) => setUniverse(e.target.value)}
-        >
-          <option value="tiles1000.hg38.bed">tiles1000.hg38.bed</option>
-          <option value="tiles1000.hg19.bed">tiles1000.hg19.bed</option>
-          <option value="tiles1000.mm10.bed">tiles1000.mm10.bed</option>
-        </select>
-      </div>
-      <div>
-        <label className="font-bold">2. Paste bedfile</label>
-        <textarea
-          placeholder="Paste your bed file here"
-          className="w-full h-96 border-2 border-black rounded-md p-2"
-          value={inputBed}
-          onChange={(e) => setInputBed(e.target.value)}
+        <input
+          type="file"
+          className="border-2 border-black rounded-md p-2"
+          onChange={(e) => {
+            const file = e.target.files[0];
+            const reader = new FileReader();
+            reader.onload = (e) => {
+              const text = e.target.result;
+              setUniverseFile(text.toString());
+            };
+            reader.readAsText(file);
+          }}
         />
       </div>
-      <div className="flex flex-row">
+      <div>
+        <label className="font-bold">2. Select bedfile</label>
+        <input
+          type="file"
+          className="border-2 border-black rounded-md p-2"
+          onChange={(e) => {
+            const file = e.target.files[0];
+            const reader = new FileReader();
+            reader.onload = (e) => {
+              const text = e.target.result;
+              setInputBed(text.toString());
+            };
+            reader.readAsText(file);
+          }}
+        />
+      </div>
+      {/* <div className="flex flex-row gap-2">
         <button
           onClick={() => runCount()}
           className="shadow-inner bg-green-400 border-2 border-black hover:bg-green-500 text-gray-800 font-bold py-2 px-4 rounded-md inline-flex items-center"
         >
           Click me
         </button>
+      </div> */}
+      <div className="flex flex-col">
+        <label className="font-bold">3. Results</label>
       </div>
     </main>
   );
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,12 @@
 mod utils;
 
+use std::{path::Path, convert::TryFrom};
+
 use wasm_bindgen::prelude::*;
+
+use genimtools::tokenizers::{TreeTokenizer, Tokenizer};
+use genimtools::common::utils::extract_regions_from_bed_file;
+use genimtools::common::models::Region;
 
 #[wasm_bindgen]
 pub fn greet(name: &str) -> String {
@@ -17,3 +23,25 @@ pub fn count_regions(bed_file: &str) -> u32 {
     count
 }
 
+#[wasm_bindgen]
+pub fn tokenize_regions(universe: &str, regions: &str) -> String {
+    let universe_path = Path::new(universe);
+    let tokenizer = TreeTokenizer::try_from(universe_path).unwrap();
+
+    let regions_path = Path::new(regions);
+    let regions_to_tokenize = extract_regions_from_bed_file(regions_path).unwrap();
+
+    let mut tokens: Vec<Region> = Vec::new();
+    for region in regions_to_tokenize.iter() {
+        let tokenized_regions = tokenizer.tokenize_region(region).unwrap();
+        tokens.extend(tokenized_regions.regions);
+    }
+
+    // join tokens by chr, start, and end into one string with newlines
+    let mut tokens_string = String::new();
+    for token in tokens.iter() {
+        tokens_string.push_str(&format!("{}\t{}\t{}\n", token.chr, token.start, token.end));
+    }
+
+    tokens_string
+}
